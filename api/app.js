3990bd60aad1542c35ab6b85f54ba987b62eeec5
kasimugur <kasiugur7@gmail.com>
2024-11-19 11:50:01 +0300

    env added

--- api/app.js
+++ api/app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV != "production")
+  require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
