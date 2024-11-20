7b6d09f9c21b9684cbeb1fa1b0436153a77bd46b
kasimugur <kasiugur7@gmail.com>
2024-11-20 17:30:18 +0300

    database connected

---
+++ api/db/Database.js
@@ -0,0 +1,34 @@
+const mongosse = require("mongoose");
+
+let instance = null
+
+class Database {
+
+  constructor() {
+    if (!instance) {
+      this.mongoConnection = null;
+      instance = this;
+    }
+
+    return instance;
+  }
+
+  async connect(options) {
+
+    try {
+      console.log('DB Connecting...');
+      let db = await mongosse.connect(options.CONNECTION_STRING);
+
+      this.mongoConnection = db;
+      console.log('DB Connected.');
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+  }
+
+
+}
+
+module.exports = Database;
